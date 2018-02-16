06b5035e8e4b32fa97fd5aa580e772033f71e0a7
root <root@DESKTOP-HRU6D6F.localdomain>
2018-02-16 20:11:40 +0100

    Initial commit

---
+++ src/main/java/com/ag04/Feeddit/FeedditApplication.java
@@ -0,0 +1,12 @@
+package com.ag04.Feeddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedditApplication.class, args);
+	}
+}
